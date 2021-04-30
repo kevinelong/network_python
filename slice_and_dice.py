b18f035874304022a665985c14d439d22cc804ac
kevinelong <kevinelong@gmail.com>
2021-04-30 13:34:19 -0700

    comments on slices

--- slice_and_dice.py
+++ slice_and_dice.py
@@ -1,7 +1,8 @@
 text = "Now is the time for all good people to come to the aid of their planet."
 words = text.split(" ")
 
-print(words[:])
-print(words[5:])
-print(words[:5])
-print(words[5:6])
+print(words[:])  # COPY ALL
+print(words[5:])  # first five
+print(words[:5])  # last five
+print(words[5:7])  # start at 5 stop before 7
+print(words[::-1])  # REVERSE
